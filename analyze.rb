db0a0ad0ab913745f8138c2471a55e65de330882
David J. Brenes <davidjbrenes@gmail.com>
2013-02-17 21:06:37 +0100

    Getting followers for each retweeter

--- analyze.rb
+++ analyze.rb
@@ -10,4 +10,19 @@ end
 
 retweets.sort_by!(&:created_at)
 
-puts retweets.map{|t| [t[:created_at], t[:user][:screen_name]].join("\t")}.join("\n")
+# Now we get the followers of each retweeter so we can make a first approximation of which path the tweet may have followed
+retweeter_ids = retweets.map{|t| t[:user][:id]}
+
+retweeter_followers = {}
+
+VCR.use_cassette('followers', record: :new_episodes) do
+  retweeter_ids.each do | retweeter_id |
+
+    followers = Twitter.follower_ids(retweeter_id).to_a
+    retweeter_followers[retweeter_id] = followers & retweeter_ids
+    puts "#{retweeter_id} -> #{retweeter_followers[retweeter_id]}"
+
+  end
+end
+
+puts retweets.map{|t| [t[:created_at], t[:user][:id], t[:user][:screen_name], retweeter_followers[t[:user][:id]]].join("\t")}.join("\n")
