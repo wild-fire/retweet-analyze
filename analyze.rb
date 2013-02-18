c61ef4a88dd45313894ffac84b77916d0009b026
David J. Brenes <davidjbrenes@gmail.com>
2013-02-18 20:04:35 +0100

    Now we take into account the API rate limitation
    We can only make 15 requests every window (15 minutes) so we get stuck in a loop until we get all the followers info

--- analyze.rb
+++ analyze.rb
@@ -12,17 +12,27 @@ retweets.sort_by!(&:created_at)
 
 # Now we get the followers of each retweeter so we can make a first approximation of which path the tweet may have followed
 retweeter_ids = retweets.map{|t| t[:user][:id]}
+retweeter_ids << retweets.first[:retweeted_status][:user][:id]
 
 retweeter_followers = {}
 
 VCR.use_cassette('followers', record: :new_episodes) do
-  retweeter_ids.each do | retweeter_id |
+  retweeter_ids.each_with_index do |retweeter_id, i |
 
-    followers = Twitter.follower_ids(retweeter_id).to_a
-    retweeter_followers[retweeter_id] = followers & retweeter_ids
-    puts "#{retweeter_id} -> #{retweeter_followers[retweeter_id]}"
+    pending = true
+    while pending
+      begin
+        followers = Twitter.follower_ids(retweeter_id).to_a
+        retweeter_followers[retweeter_id] = followers & retweeter_ids
+        pending = false
+      rescue Exception => e
+        puts e.message
+        sleep(60)
+      end
+    end
+
 
   end
 end
 
-puts retweets.map{|t| [t[:created_at], t[:user][:id], t[:user][:screen_name], retweeter_followers[t[:user][:id]]].join("\t")}.join("\n")
+puts retweets.map{|t| [t[:created_at], t[:user][:id], t[:user][:screen_name], retweeter_followers[t[:user][:id]].inspect].join("\t")}.join("\n")
