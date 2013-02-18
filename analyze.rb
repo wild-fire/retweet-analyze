241b9d69f4150ce523671523b872d8bd036b0ff5
David J. Brenes <davidjbrenes@gmail.com>
2013-02-18 21:11:52 +0100

    Filtering the followers of the retweeters so we only take into account those who hadn't retweeted yet

--- analyze.rb
+++ analyze.rb
@@ -12,7 +12,7 @@ retweets.sort_by!(&:created_at)
 
 # Now we get the followers of each retweeter so we can make a first approximation of which path the tweet may have followed
 retweeter_ids = retweets.map{|t| t[:user][:id]}
-retweeter_ids << retweets.first[:retweeted_status][:user][:id]
+retweeter_ids.unshift retweets.first[:retweeted_status][:user][:id]
 
 retweeter_followers = {}
 
@@ -35,4 +35,16 @@ VCR.use_cassette('followers', record: :new_episodes) do
   end
 end
 
-puts retweets.map{|t| [t[:created_at], t[:user][:id], t[:user][:screen_name], retweeter_followers[t[:user][:id]].inspect].join("\t")}.join("\n")
+# Now we loop through the retweets and discard the followers that already had retweeted as they couldn't get influenced by further retweets
+pending_retweeters = retweets.map{|t| t[:user][:id]}
+
+retweets.each do |retweet|
+
+  influenced_retweeters = retweeter_followers[retweet[:user][:id]] & pending_retweeters
+
+  puts [retweet[:created_at], retweet[:user][:id], retweet[:user][:screen_name], influenced_retweeters.inspect].join("\t")
+
+  pending_retweeters.delete retweet[:user][:id]
+
+end
+
